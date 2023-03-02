eedf8edeeeea3fcba31b3e3d8edaa90550f72a5f
TylerSyn <tcsynott@email.neit.edu>
2023-03-02 12:12:22 -0500

    gkj

--- GDS_126_Engine/js/Data/player.js
+++ GDS_126_Engine/js/Data/player.js
@@ -19,33 +19,33 @@ Properties:
 
 var playerData ={
 	info:{
-		src:`images/snale.png`
+		src:`images/rotospritesheet.png`
 	},
 	states:{
 		//The idle animation 
     	idle:
 		{
-			fps:1,
+			fps:15,
 			cycle:true,
 			frames:
 			[
-				{width:128, height:128, startX:0, startY:0},
-				{width:128, height:128, startX:128, startY:0}
+				{width:512, height:512, startX:0, startY:0},
+				{width:512, height:512, startX:512, startY:0}
 				
 			]
 		},
 		//The walwidth:128, height:128,
 		walk:
 		{
-			fps:1,
+			fps:15,
 			cycle:true,
 			frames:
 			[
-				{width:128, height:128, startX:0, startY:0},
-				{width:128, height:128, startX:128, startY:0},
-				{width:128, height:128, startX:256, startY:0},
-				{width:128, height:128, startX:384, startY:0},
-				{width:128, height:128, startX:512, startY:0}
+				{width:512, height:512, startX:0, startY:0},
+				{width:512, height:512, startX:512, startY:0},
+				{width:512, height:512, startX:512*2, startY:0},
+				{width:512, height:512, startX:512, startY:0},
+				//{width:128, height:128, startX:512, startY:0}
 			]
 		},
 		//The jump animation 
@@ -55,7 +55,7 @@ var playerData ={
 			cycle:false,
 			frames:
 			[
-				{width:128, height:128, startX:640, startY:0}
+				{width:512, height:512, startX:512*4, startY:0}
 			]
 		},
 		//The crouch animation 
@@ -65,11 +65,11 @@ var playerData ={
 			cycle:true,
 			frames:
 			[
-				{width:128, height:128, startX:768, startY:0},
-				{width:128, height:128, startX:768, startY:0},
-				{width:128, height:128, startX:768, startY:0},
-				{width:128, height:128, startX:768, startY:0},
-				{width:128, height:128, startX:896, startY:0}
+				{width:512, height:512, startX:512*5, startY:0},
+			//	{width:128, height:128, startX:768, startY:0},
+				//{width:128, height:128, startX:768, startY:0},
+				//{width:128, height:128, startX:768, startY:0},
+				//{width:128, height:128, startX:896, startY:0}
 			]
 		},
 		//The attack animation 
@@ -80,13 +80,13 @@ var playerData ={
 			//width:300,
 			frames:
 			[
-				{width:128, height:128, startX:1024, startY:0},
-				{width:128, height:128, startX:1024, startY:0},
-				{width:128, height:128, startX:1024, startY:0},
-				{width:128, height:128, startX:1152, startY:0},
-				{width:128, height:128, startX:1152, startY:0},
-				{width:128, height:128, startX:1152, startY:0},
-				{width:128, height:128, startX:1152, startY:0}
+				{width:512, height:512, startX:512*3, startY:0},
+				//{width:128, height:128, startX:1024, startY:0},
+				//{width:128, height:128, startX:1024, startY:0},
+				//{width:128, height:128, startX:1152, startY:0},
+			//	{width:128, height:128, startX:1152, startY:0},
+			//	{width:128, height:128, startX:1152, startY:0},
+				//{width:128, height:128, startX:1152, startY:0}
 				
 			]
 		}
