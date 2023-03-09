efd2008fc7108502c0ce6d8ae0e7954e619904fd
TylerSyn <tcsynott@email.neit.edu>
2023-03-09 12:09:39 -0500

    bullets added in (implemenytysdjhgbuhr o)

---
+++ GDS_126_Engine/js/Data/bullet.js
@@ -0,0 +1,40 @@
+/*----------------------------------------------
+This file contains the data used to render the player's sprites
+Properties:
+	Object: info | information about the sprite file
+		String: info.src | The location of the spritesheet
+	Object: states | contains the data for each animation state
+		Object: [`state name`] | The data used to render the idle state
+			Number: fps | The frame rate in which to render the animation
+			Boolean: cycle | Whether or not the animation will loop
+			Array: frames| Contains objects with geometric data for each frame of animtati.
+					Must contain at least one frame object.
+					The animation will run for however many frame objects are added to the array
+				Object: [index number] | A frame of animation
+					Number: width | the actual 1/1 horizontal size of the portion of image file to be rendered
+					Number: height | the actual 1/1 vertical size of the portion of image file to be rendered
+					Number: startX | the horizontal starting point of the portion of image file to be rendered
+					Nunber: startY | the vertical starting point of the portion of image file to be rendered
+/*----------------------------------------------*/
+
+var bulletData ={
+	info:{
+		src:`images/projectile spritesheet.png`
+	},
+	states:{
+		//The idle animation 
+    	idle:
+		{
+			fps:15,
+			cycle:true,
+			frames:
+			[
+				{width:256, height:256, startX:0, startY:0},
+				{width:256, height:256, startX:256, startY:0},
+				{width:256, height:256, startX:256*2, startY:0}
+				
+			]
+		}
+
+    }
+}
