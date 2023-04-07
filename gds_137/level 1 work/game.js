db42f86670fdd68d4b640e8a144c819fbb88fe97
TylerSyn <tcsynott@email.neit.edu>
2023-04-07 11:39:07 -0400

    color changing

--- gds_137/level 1 work/game.js
+++ gds_137/level 1 work/game.js
@@ -22,7 +22,10 @@ function animate()
 context.save();
 	context.clearRect(0,0,canvas.width, canvas.height);	
 	
-
+function randomRange(high,low)
+{
+	return Math.random() * (high-low)+low;
+}
 
   ball.move();
 
@@ -30,24 +33,28 @@ context.save();
 	{
 		ball.x = 0 + ball.width/2;
 		ball.vx= ball.vx * -1;
+		ball.color = `rgb(${randomRange(255,0)},${randomRange(255,0)},${randomRange(255,0)})`
 	}
 
 	if(ball.x > canvas.width - ball.width/2)
 	{
 		ball.x == canvas.width - ball.width/2;
 		ball.vx= ball.vx * -1;
+		ball.color = `rgb(${randomRange(255,0)},${randomRange(255,0)},${randomRange(255,0)})`
 	}
 
 	if(ball.y < 0 + ball.height/2)
 	{
 		ball.y = 0 + ball.height/2;
 		ball.vy= ball.vy * -1;
+		ball.color = `rgb(${randomRange(255,0)},${randomRange(255,0)},${randomRange(255,0)})`
 	}
 
 	if(ball.y > canvas.height - ball.height/2)
 	{
 		ball.y == canvas.height - ball.height/2;
 		ball.vy= ball.vy * -1;
+		ball.color = `rgb(${randomRange(255,0)},${randomRange(255,0)},${randomRange(255,0)})`
 	}
 
 	
