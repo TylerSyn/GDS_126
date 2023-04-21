9c57433cba9afa1c9768c1e235dec9848a6a57c4
TylerSyn <tcsynott@email.neit.edu>
2023-04-21 12:17:46 -0400

    buyh

---
+++ levels 1-3 boss/controls.js
@@ -0,0 +1,36 @@
+var a = false;
+var d = false;
+
+
+document.addEventListener("keydown", press);
+document.addEventListener("keyup", release);
+
+
+function press(e)
+{
+
+	
+	if(e.keyCode == 65)
+	{
+		a = true;
+	}
+	if(e.keyCode == 68)
+	{
+		d = true;
+	}
+}
+
+function release(e)
+{
+	//---This logs key codes into the browser's console.
+	//console.log("Released" + e.keyCode);
+	
+	if(e.keyCode == 65)
+	{
+		a = false;
+	}
+	if(e.keyCode == 68)
+	{
+		d = false;
+	}
+}
